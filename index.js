98160ff745e17a31baa66201e0730037dbc35219
TerrenceBell <terrencekbell@gmail.com>
2020-06-24 13:40:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, func){
+    let mappedArray = []
+    array.forEach(index => {
+        mappedArray.push(func(index))
+    })
+    return mappedArray;
+}
+
+function reduce(array, func, start){
+    let accumulator;
+    let index;
+
+    if(!!start){
+        accumulator = start;
+        index = 0;
+    } else {
+        accumulator = func(array[0],array[1]);
+        index = 2;
+    }
+    for(index; index< array.length; index++){
+        accumulator = func(accumulator, array[index])
+    }
+   return accumulator
+}
