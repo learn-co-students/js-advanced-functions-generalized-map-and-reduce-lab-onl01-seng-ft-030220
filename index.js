08d8c6f0acd889e4ad35e3f1822581f30077ff6b
winifredf <winifred.dykes@hotmail.com>
2020-08-25 01:34:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+const map = (array, callback) => {
+    const new_array = []
+    for (let elem of array) {
+        new_array.push(callback(elem))
+    }
+    return new_array
+}
+
+const reduce = (array, callback, start) => {
+    if (start === undefined) {
+        let accum = array[0]
+        for (let i = 1, i < array.length; i++) {
+            accum = callback(accum, array[i])
+        }
+        return accum
+    } 
+    else {
+        let accum = start
+        for (let i = 0; i < array.length; i++) {
+            accum = callback(accum, array[i])
+        }
+        return accum
+    }
+}
