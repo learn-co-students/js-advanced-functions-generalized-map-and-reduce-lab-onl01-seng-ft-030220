6d0cb5fbf202b27704464d7a006e7e48b3e89b4d
marsha <marshaannon@gmail.com>
2020-10-28 21:20:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(arr, mapp) {
+    let newArr = [];
+    
+    for (let x of arr) {
+        newArr.push(mapp(x));
+    }
+    return newArr;
+}
+
+function reduce(arr, reducer, start = arr[0]) {
+    let total = start;
+
+    for (let x of arr) {
+        if (x !== arr[0] || start !== arr[0]) {
+            total = reducer(x, total);
+        }
+    }
+    return total;
+}
